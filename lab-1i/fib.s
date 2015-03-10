b6033a6e2ff5416a23815286b91ff4cf7b4aaf7a
Will Fong <fongwh1016@gmail.com>
2015-03-10 11:01:30 +0800

    fib.s: correct indentation to meet the coding style
    	tab=4 space ===> tab=tab

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,19 +12,19 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-    cmp r0, #2
-    bmi RET
-    mov r1, #0
-    mov r2, #1
+	cmp r0, #2
+	bmi RET
+	mov r1, #0
+	mov r2, #1
 Loop:
-    add r1, r1, r2
-    eors r1, r1, r2
-    eors r2, r1, r2
-    eors r1, r2, r1
-    sub r0, r0, #1
-    cmp r0, #1
-    bne Loop
-    mov r0, r2
+	add r1, r1, r2
+	eors r1, r1, r2
+	eors r2, r1, r2
+	eors r1, r2, r1
+	sub r0, r0, #1
+	cmp r0, #1
+	bne Loop
+	mov r0, r2
 RET:
 	pop {r3, r4, r5, pc}		@EPILOG
 
